be6338dde03e168565c1d74bb8c7ce00b8a4cf83
Pre-11 <prernakumari7427@gmail.com>
2023-08-30 20:15:31 +0530

    routes added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,21 @@
 
 import './App.css';
+import{BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Login from './Component/Login';
+import Signup from './Component/Signup';
+import Home from './Component/Home';
+import {useState} from 'react'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path ="/" element ={<Login />}/>
+          <Route path ="/signup" element ={<Signup/>}/>
+          <Route path ="/home" element ={<Home />}/>
+        </Routes>
+      </Router>
      
     </div>
   );
---
+++ myapp/src/Component/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+        <form>
+            <h1>signup</h1>
+        </form>
+
+    </div>
+  )
+}
+
+export default Signup
