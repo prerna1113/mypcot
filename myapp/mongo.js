38a2037d97ebe4e063aa6370140533cc211f6b83
Pre-11 <prernakumari7427@gmail.com>
2023-08-30 21:00:44 +0530

    mongo added

--- myapp/mongo.js
+++ myapp/mongo.js
@@ -8,3 +8,20 @@ mongoose.connect("mongodb://localhost:27017/react-login")
 .catch(()=>{
     console.log('failed')
 })
+
+
+
+const newSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports = collection
